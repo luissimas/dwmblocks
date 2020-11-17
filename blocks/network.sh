688ed31fec39f9a6d37a398d7407ec1ed81a344e
padawan <luissimas@protonmail.com>
2020-11-17 17:21:01 -0300

    adding wifi to network block

--- blocks/network.sh
+++ blocks/network.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 ethStatus="$(cat /sys/class/net/enp0*/operstate)"
-# wifiStatus="$(cat /sys/class/net/w*/operstate)"
+wifiStatus="$(cat /sys/class/net/w*/operstate)"
+wifiName="$(iwgetid -r)"
 
 if [ $ethStatus = "up" ]; then
 	printf " Ethernet"
+elif [ $wifiStatus = "up" ]; then
+  printf  " $wifiName"
 else
 	printf " Disconnected"	
 fi
