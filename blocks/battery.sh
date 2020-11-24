ddfabbde334a211b23623aca37e44e0a993acd3e
padawan <luissimas@protonmail.com>
2020-11-23 21:24:51 -0300

    reverting battery script fix

--- blocks/battery.sh
+++ blocks/battery.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-battery"$(cat /sys/class/power_supply/BAT* | wc -l)"
 batteryStatus="$(cat /sys/class/power_supply/BAT*/status)"
 capacity="$(cat /sys/class/power_supply/BAT*/capacity)"
 
-if [ $battery = "1" ]; then
 
-  if [ $batteryStatus = "Charging" ]; then
-    icon=" "
-  elif [ $capacity -ge 75 ]; then
-    icon=" "
-  elif [ $capacity -lt 75 ] && [ $capacity -ge 50 ]; then
-    icon=" "
-  elif [ $capacity -lt 50 ] && [ $capacity -ge 25 ]; then
-    icon=" "
-  elif [ $capacity -lt 25 ] && [ $capacity -ge 0 ]; then
-    icon=" "
-  fi
-
-  printf "$icon $capacity%%"
-
+if [ $batteryStatus = "Charging" ]; then
+  icon=" "
+elif [ $capacity -ge 75 ]; then
+  icon=" "
+elif [ $capacity -lt 75 ] && [ $capacity -ge 50 ]; then
+  icon=" "
+elif [ $capacity -lt 50 ] && [ $capacity -ge 25 ]; then
+  icon=" "
+elif [ $capacity -lt 25 ] && [ $capacity -ge 0 ]; then
+  icon=" "
 fi
+
+printf "$icon $capacity%%"
+
