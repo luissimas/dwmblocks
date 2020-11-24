92d8ed112848c3be77456a308557f05aab25fc73
pandawan69 <luissimas@protonmail.com>
2020-11-23 21:15:34 -0300

    adding conditional statement to display battery only if the device has battery

--- blocks/battery.sh
+++ blocks/battery.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-status="$(cat /sys/class/power_supply/BAT*/status)"
+acStatus"$(cat /sys/class/power_supply/AC/status)"
+batteryStatus="$(cat /sys/class/power_supply/BAT*/status)"
 capacity="$(cat /sys/class/power_supply/BAT*/capacity)"
 
-if [ $status = "Charging" ]; then
+if [ $acStatus = "1" ]; then
+
+elif [ $batteryStatus = "Charging" ]; then
 	icon=" "
 elif [ $capacity -ge 75 ]; then
 	icon=" "
@@ -14,5 +17,3 @@ elif [ $capacity -lt 50 ] && [ $capacity -ge 25 ]; then
 elif [ $capacity -lt 25 ] && [ $capacity -ge 0 ]; then
   icon=""
 fi
-
-printf "$icon $capacity%%"
