57fc3f17980f387d741f81d8420f91327874fb32
padawan <luissimas@protonmail.com>
2020-12-25 17:08:20 -0300

    Increased interval for the packages script

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", 		"~/repos/dwmblocks/blocks/packages.sh", 	60, 			0},
+	{"", 		"~/repos/dwmblocks/blocks/packages.sh", 	3600, 			0},
 
 	{"", 		"~/repos/dwmblocks/blocks/temp.sh", 	2, 			0},
 
