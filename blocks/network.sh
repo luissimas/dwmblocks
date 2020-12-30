4801c2ff6ff46ccbc08d62f765b0d057c051385d
padawan <luissimas@protonmail.com>
2020-12-30 10:28:01 -0300

    Fixing network and packages script

--- blocks/network.sh
+++ blocks/network.sh
@@ -4,9 +4,9 @@ ethStatus="$(cat /sys/class/net/enp0*/operstate)"
 wifiStatus="$(cat /sys/class/net/w*/operstate)"
 wifiName="$(iwgetid -r)"
 
-if [ $ethStatus = "up" ]; then
+if [[ $ethStatus = "up" ]]; then
 	printf " Ethernet"
-elif [ $wifiStatus = "up" ]; then
+elif [[ $wifiStatus = "up" ]]; then
   printf  "  $wifiName"
 else
 	printf " Disconnected"	
--- blocks/packages.sh
+++ blocks/packages.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Sync mirrors to check for updates
+sudo pacman -Sy >/dev/null
+
 packagesCount="$(pacman -Qu | wc -l)"
 
-if [[ packagesCount -gt 0 ]] then
+if [[ packagesCount -gt 0 ]]; then
   printf "  $packagesCount"
 fi
