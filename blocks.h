77396f5cd68fe4866fca418f119ae16e9f16c6ed
padawan <luissimas@protonmail.com>
2021-01-31 12:45:54 -0300

    Moving scripts to scripts home folder

--- blocks.h
+++ blocks.h
@@ -1,21 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", 		"~/repos/dwmblocks/blocks/packages.sh", 	3600, 			0},
+	{"", 		"~/scripts/bar/packages.sh", 	3600, 			0},
 
-	{"", 		"~/repos/dwmblocks/blocks/temp.sh", 	2, 			0},
+	{"", 		"~/scripts/bar/temp.sh", 	2, 			0},
 
-	{"", 		"~/repos/dwmblocks/blocks/network.sh", 	5, 			0},
+	{"", 		"~/scripts/bar/network.sh", 	5, 			0},
 
-//	{"", 		"~/repos/dwmblocks/blocks/memory.sh", 	5, 			0},
+//	{"", 		"~/scripts/bar/memory.sh", 	5, 			0},
 
-	{"", 		"~/repos/dwmblocks/blocks/volume.sh", 	0, 			10},
+	{"", 		"~/scripts/bar/volume.sh", 	0, 			10},
 
-	{"", 		"~/repos/dwmblocks/blocks/battery.sh", 	10, 			0},
+	{"", 		"~/scripts/bar/battery.sh", 	10, 			0},
 
-	{"", 		"~/repos/dwmblocks/blocks/date.sh", 	60, 			0},
+	{"", 		"~/scripts/bar/date.sh", 	60, 			0},
 
-	{"", 		"~/repos/dwmblocks/blocks/time.sh", 	30, 			0},
+	{"", 		"~/scripts/bar/time.sh", 	30, 			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
--- blocks/temp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-temp="$(sensors | grep temp1 | awk 'NR==1{printf "%d", $2}')"
-
-printf "﬙ $temp°C"
